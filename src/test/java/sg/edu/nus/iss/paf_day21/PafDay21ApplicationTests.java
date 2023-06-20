198f5062b7fe79dcad6b0fbd517294cf4300040b
crystaleemj <ceemj_@hotmail.com>
2023-06-20 12:26:27 +0800

    first commit

---
+++ src/test/java/sg/edu/nus/iss/paf_day21/PafDay21ApplicationTests.java
@@ -0,0 +1,13 @@
+package sg.edu.nus.iss.paf_day21;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PafDay21ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
